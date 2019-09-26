3b6a12556ec5ac3799c44414da424f9e319ebf6b
Areydra Desfikriandre <areydra@gmail.com>
2019-09-26 07:52:18 +0700

    refactor

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,7 +11,7 @@ module.exports = {
             const checkPassword = await bcrypt.compare(req.body.password, user.password)
             
             if(checkPassword){
-                response.success(res, 200, { status: 200, message: 'success login', id_level: user.id_level, token: token })
+                response.success(res, 200, { status: 200, message: 'success login', user: user, id_level: user.id_level, token: token })
             }else{
                 response.failed(res, 400, NULL, { status: 400, message: 'invalid email or password' })
             }
@@ -30,7 +30,7 @@ module.exports = {
             userModel.getUserByEmail(user.email).then(async responses => {
                 const newUser = responses[0]
                 const token = jwt.sign({ id: newUser.id, email: newUser.email, id_level: newUser.id_level }, process.env.SECRET_KEY, { expiresIn: '24h' })
-                response.success(res, 200, user, { token:token })
+                response.success(res, 200, user, { token:token, user: newUser })
             })
         }).catch(err => {
             console.log(err)
